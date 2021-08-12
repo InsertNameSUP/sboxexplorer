552c00781b435b0cb22d59d63a40f2f1866d25af
InsertNameSUP <Insert.NameSUP@gmail.com>
2021-08-12 22:43:36 +1000

    Credits Page & Google Verif.

---
+++ credits/content.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch("https://CORS.insert-name.repl.co/https:/api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D060F6B1C706DABC0E97B1FACD926023&steamids=76561198161943355,76561198057710762").then(data=>data.json())
+    .then((steamUsers) => {
+    steamUsers = steamUsers.response.players;
+    console.log(steamUsers);
+    creditElems = document.getElementsByClassName("credit-box");
+    for(var i = 0; i < steamUsers.length; i++) {
+        steamUser = steamUsers[i];
+        creditElems[i].innerHTML = `
+        <h2 style="text-align:center;">
+            <img src="${steamUser.avatarmedium}" style="margin: auto;">
+            <br>
+            ${steamUser.personaname}
+        </h2>
+        <p class="contributions"></p>
+        `;
+        creditElems[i].href = steamUser.profileurl;
+        if(steamUser.steamid == "76561198057710762") document.getElementsByClassName("contributions")[i].innerHTML = `Current Version of Sbox Explorer`;
+        if(steamUser.steamid == "76561198161943355") document.getElementsByClassName("contributions")[1].innerHTML = `Depricated Version of <a class="hyper" href="https://explorer.sboxed.com/">Sbox Explorer</a> (Original)`;
+    }
+    })
+});
